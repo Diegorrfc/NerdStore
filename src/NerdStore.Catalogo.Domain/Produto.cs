78b98311227b4e59176881bafab6365661cb6d4b
Diego Rodrigo <diego.rrfc@gmail.com>
2020-04-05 16:52:45 -0300

    Melhorando Domonio Produto

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using NerdStore.Core.DomainObject;
+
 namespace NerdStore.Catalogo.Domain
 {
-    public class Produto
+    public class Produto : Entity, IAggregateRoot
     {
-        public Guid Id { get; set; }
+        public Produto(string nome, string descricao, bool ativo, decimal valor, DateTime dataCadastro, string imagem, Guid categoriaID)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            CategoriaID = categoriaID;
+        }
+
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Guid CategoriaID { get; set; }
+        public Categoria Categoria { get; private set; }
+
+        public void Ativar() => Ativo = true;
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaID = categoria.Id;
+        }
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
     }
-    public class Categoria
+   
+    public class Categoria : Entity
     {
-        public Guid Id { get; set; }
+
     }
 }
---
+++ src/NerdStore.Core/DomainObject/IAggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace NerdStore.Core.DomainObject
+{
+    public interface IAggregateRoot
+    {        
+    }
+}
