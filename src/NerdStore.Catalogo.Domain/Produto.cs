b1d17054c92a75641dc8cde037b03a2aa61b5556
Diego Rodrigo <diego.rrfc@gmail.com>
2020-04-05 15:54:28 -0300

    Criação entity

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+    }
+    public class Categoria
+    {
+        public Guid Id { get; set; }
+    }
+}
