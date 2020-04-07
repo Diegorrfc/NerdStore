9ee4faf9f0a0a3e83d90a2449dee80e5a0e9e5ee
Diego Rodrigo <diego.rrfc@gmail.com>
2020-04-07 02:52:09 -0300

    Ajustes

---
+++ src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }            
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            const string propertyToCommit = "DataCadastro";
+
+            foreach (var item in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty(propertyToCommit) != null))
+            {
+                if (item.State == EntityState.Added)
+                    item.Property(propertyToCommit).CurrentValue = DateTime.Now;
+
+                if (item.State == EntityState.Modified)
+                    item.Property(propertyToCommit).IsModified = false;
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Data/Mapping/ProdutoMapping.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mapping
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(p => p.Descricao)
+              .IsRequired()
+              .HasColumnType("varchar(500)");
+
+            builder.Property(p => p.Imagem)
+              .IsRequired()
+              .HasColumnType("varchar(250)");
+
+            builder.OwnsOne(p => p.Dimensoes, dm =>
+            {
+                dm.Property(d => d.Altura)
+                .HasColumnName("Altura")
+                .HasColumnType("int");
+
+                dm.Property(d => d.Largura)
+               .HasColumnName("Largura")
+               .HasColumnType("int");
+
+                dm.Property(d => d.Profundidade)
+               .HasColumnName("Profundidade")
+               .HasColumnType("int");
+
+            });
+
+            builder.ToTable("Produtos");
+
+        }
+    }
+}
